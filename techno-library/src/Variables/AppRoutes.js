5d985d62bcd822dad0fcbadec82e9b24f49c0287
djk04 <djk04@mail.aub.edu>
2022-04-23 00:40:36 +0300

    Added SignUp Page

--- techno-library/src/Variables/AppRoutes.js
+++ techno-library/src/Variables/AppRoutes.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Authors from '../Pages/Authors';
-import Home from '../Pages/Home';
-import Categories from '../Pages/Categories';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Authors from "../Pages/Authors";
+import Home from "../Pages/Home";
+import Categories from "../Pages/Categories";
 import Login from "../Pages/Login";
-// import SignUp from "../Pages/SignUp";
+import SignUp from "../Pages/SignUp";
 
 const AppRoutes = () => {
   return (
     <>
-        <Router>
-            <Routes>
-            <Route exact path="/" element={<Home />} />
-        <Route exact path="/Home" element={<Home />} />
-          <Route exact path="/login" element={<Login/>}/>
-          {/* <Route exact path="/signup" element={<SignUp/>}/> */}
-          <Route exact path="/categories" element={<Categories/>}/>
-                <Route exact path='/Authors' element={<Authors/>} />
-            </Routes>
-        </Router>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/Home" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<SignUp />} />
+          <Route exact path="/categories" element={<Categories />} />
+          <Route exact path="/Authors" element={<Authors />} />
+        </Routes>
+      </Router>
     </>
-  )
-}
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
