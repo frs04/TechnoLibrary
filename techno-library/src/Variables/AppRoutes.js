59db24ac15ed6598df22149705370c33021cb499
djk04 <djk04@mail.aub.edu>
2022-04-23 00:38:03 +0300

    Added Logo to browser + Categories Page + linked pages to navbar

--- techno-library/src/Variables/AppRoutes.js
+++ techno-library/src/Variables/AppRoutes.js
@@ -2,13 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Authors from '../Pages/Authors';
 import Home from '../Pages/Home';
+import Categories from '../Pages/Categories';
+// import Login from "../Pages/Login";
+// import SignUp from "../Pages/SignUp";
 
 const AppRoutes = () => {
   return (
     <>
         <Router>
             <Routes>
-                <Route exact path='/' element={<Home/>} />
+            <Route exact path="/" element={<Home />} />
+        <Route exact path="/Home" element={<Home />} />
+          {/* <Route exact path="/login" element={<Login/>}/>
+          <Route exact path="/signup" element={<SignUp/>}/> */}
+          <Route exact path="/categories" element={<Categories/>}/>
                 <Route exact path='/Authors' element={<Authors/>} />
             </Routes>
         </Router>
